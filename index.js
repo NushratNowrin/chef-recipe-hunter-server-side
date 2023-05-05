26bb8e8160d47c1f24e114211776a41563c8fbd5
NushratNowrin <nowrin2023@gmail.com>
2023-05-05 20:13:21 +0600

    single chef route done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const chefs = require('./chef.json')
+const recipes = require('./recipe.json')
 const app = express()
 app.use(cors())
 const port = 5000
@@ -11,6 +12,16 @@ app.get('/', (req, res) => {
 
 app.get('/chefs', (req, res) => {
   res.send(chefs)
+})
+
+app.get('/chefs/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const chef = chefs.find(chef => chef.id === id) || {} ;
+  res.send(chef)
+})
+
+app.get('/recipes', (req, res) => {
+  res.send(recipes)
 })
 
 app.listen(port, () => {
